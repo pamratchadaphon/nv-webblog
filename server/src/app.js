b13931fca8a0e995cc7391b0aae02cdd1b48026e
pamratchadaphon <139990701+pamratchadaphon@users.noreply.github.com>
2023-08-09 09:23:53 +0700

    second commit

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,26 @@
-console.log('Helo nodejs server');
+let express = require('express')
+
+const app = express()
+
+app.get('/status', function (req, res ){
+    res.send('Hello nodejs server')
+})
+
+app.get('/hello/:person', function (req,res){
+    console.log('hello - '+ req.params.person);
+})
+
+// get user by id
+app.get('/user/:userId', function(req,res){
+    res.send('ดูข้อมูลผู้ใช้งาน')
+})
+
+// get all user
+app.get('/users', function(req,res){
+    res.send('เรียกดูข้อมูลผู้ใช้งาน')
+})
+
+let port = 8081
+app.listen(port,function(){
+    console.log('server running on '+port);
+})
