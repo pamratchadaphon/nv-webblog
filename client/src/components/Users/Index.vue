505ca2898362ec333e4471bf1e98077948c9dc26
pamratchadaphon <139990701+pamratchadaphon@users.noreply.github.com>
2023-09-27 11:53:53 +0700

    page147

--- client/src/components/Users/Index.vue
+++ client/src/components/Users/Index.vue
@@ -1,14 +1,53 @@
 <template>
   <div>
     <h1>Get All Users</h1>
+    <h4>จำนวนผู้ใช้งาน{{ users.length }}</h4>
+    <div v-for="user in users" v-bind:key="user.id">
+      <p>id: {{ user.id }}</p>
+      <p>ชื่อ-นามสกุล :{{ user.name }} - {{ user.lastname }}</p>
+      <p>email: {{ user.email }}</p>
+      <p>password: {{ user.password }}</p>
+      <p><button v-on:click="navigateTo('/user/' + user.id)">ดูข้อมูลผู้ใช้</button></p>
+      <p><button v-on:click="navigateTo('/user/edit/' + user.id)">edit user</button></p>
+      <p><button v-on:click="deleteUser(user)">ลบข้อมูล</button></p>
+      <hr />
+    </div>
   </div>
 </template>
 <script>
 import UsersService from "@/services/UsersService";
+
 export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  methods: {
+    navigateTo(route) {
+      this.$router.push(route);
+    },
+    async deleteUser(user) {
+      let result = confirm("Want to delete?");
+      if (result) {
+        try {
+          await UsersService.delete(user);
+          this.refreshData()
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    },
+    async refreshData() {
+      this.users = (await UsersService.index()).data;
+    },
+  },
   async created() {
-    let results = (await UsersService.index()).data;
-    console.log(results);
+    try {
+      this.users = (await UsersService.index()).data;
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
 </script>
--- client/src/components/Users/ShowUser.vue
+++ client/src/components/Users/ShowUser.vue
@@ -1,9 +1,36 @@
 <template>
   <div>
     <h1>Get User By Id</h1>
+    <p>id: {{ user.id }}</p>
+    <p>ชื่อ-นามสกุล :{{ user.name }} - {{ user.lastname }}</p>
+    <p>email: {{ user.email }}</p>
+    <p>password: {{ user.password }}</p>
+    <p><button v-on:click="navigateTo('/user/edit/' + user.id)">edit user</button></p>
+    <p><button v-on:click="navigateTo('/users')">กลับ</button></p>
   </div>
 </template>
 <script>
-export default {};
+import UsersService from "@/services/UsersService";
+
+export default {
+  data() {
+    return {
+      user: [],
+    };
+  },
+  methods: {
+    navigateTo(route) {
+      this.$router.push(route);
+    },
+  },
+  async created() {
+    try {
+      let userId = this.$route.params.userId;
+      this.user = (await UsersService.show(userId)).data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
 </script>
 <style scoped></style>
